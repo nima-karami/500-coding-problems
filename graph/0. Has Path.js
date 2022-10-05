88093e7c48b3619ee37ec449d22560c0f8beee1e
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-10-05 18:18:30 -0400

    has path

---
+++ graph/0. Has Path.js
@@ -0,0 +1,27 @@
+// Recursive depth first search
+const hasPathR = (graph, src, dst) => {
+    
+    if (src === dst ) return true;
+    
+    for (neighbor of graph[src]) {
+      if (hasPath(graph, neighbor, dst)) return true;
+    }
+    
+    return false;
+};
+  
+// Iterative breadth first search
+const hasPath = (graph, src, dst) => {
+    const queue = [src];
+    
+    while (queue.length > 0) {
+      let curr = queue.shift();
+      if (curr === dst) return true;
+      
+      for (neighbor of graph[curr]) {
+        queue.push(neighbor);
+      }
+    }
+    
+    return false;
+  };
